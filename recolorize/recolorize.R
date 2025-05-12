6c503d1d9e5ad618e2218770ea08a0cdae32d398
salvadormik9 <salvadormik9@gmail.com>
2025-05-12 15:49:16 -0700

    Test Run of Recolorize on More Distinct Coloration Picture

--- recolorize/recolorize.R
+++ recolorize/recolorize.R
@@ -2,10 +2,10 @@
 
 #Set my working directory to Sde_phenotyping
 
-setwd("C:/Users/salva/Downloads/Sde_phenotyping")
+setwd("C:/Users/salva/Downloads/Sde_phenotyping_new")
 
-#Install recolorize
-install.packages("recolorize")
+# #Install recolorize
+# install.packages("recolorize")
 
 #Load packages
 library(recolorize)
@@ -16,14 +16,21 @@ library(recolorize)
 
 #User Guide that Walks through the Package: https://hiweller.rbind.io/post/introduction-to-recolorize/
 
+#Before reading in the image, make sure to remove the background of the image. I used Pixlr background remover (https://pixlr.com/remove-background/), but you can also use Adobe Express (https://www.adobe.com/express/feature/image/remove-background). 
+
 # get the path to the image (comes with the package, so we use system.file):
-img <- "C:/Users/salva/Downloads/Sde_phenotyping/P7170195_bgrm_cc.png"
+img <- "C:/Users/salva/Downloads/Sde_phenotyping_new/recolorize/P7170195_input_test1_bgrm_cc.png"
+img2 <- "C:/Users/salva/Downloads/Sde_phenotyping_new/recolorize/recolorize_input_test2_bgrm_cc.png"
 
 # fit a color map
 #Parameters, bins into 64 total clusters (4^3 = 64) and cutoff similarity for grouping color centers together = 45%
-recolorize_obj <- recolorize2(img,
+recolorize_obj1 <- recolorize2(img,
                               bins = 4,
                               cutoff = 45)
+
+recolorize_obj2 <- recolorize2(img2,
+                               bins = 4, 
+                               cutoff = 45)
 
 
 
